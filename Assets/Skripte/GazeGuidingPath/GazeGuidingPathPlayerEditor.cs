93d5ec747513052866039df9444f0e6374cd6635
masmir123 <117976753+masmir123@users.noreply.github.com>
2025-01-13 14:25:50 +0100

    AusfallAnzeigen + Anfang GazeGuiding
    Alle "AufallAnzeigen" wurden hinzugefügt, und ein Skript namens "AufallAnzeigenManager", das diese Anzeigen verwaltet. Damit kann man von überall in der Szene die Lampen einschalten (Erklärung im Skript selbst).

---
+++ Assets/Skripte/GazeGuidingPath/GazeGuidingPathPlayerEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GazeGuidingPathPlayer))]
+public class GazeGuidingPathPlayerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GazeGuidingPathPlayer script = (GazeGuidingPathPlayer)target;
+        if (GUILayout.Button("Clear Line"))
+        {
+            script.ClearLine();
+        }
+
+        if (GUILayout.Button("Trigger TEST"))
+        {
+            script.triggerTEST();
+        }
+    }
+}
