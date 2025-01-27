a198d673443f721fa392a303464c3dae866f2376
masmir123 <117976753+masmir123@users.noreply.github.com>
2025-01-27 10:35:37 +0100

    Port -> 8080

--- Assets/Skripte/GlobalConfig.cs
+++ Assets/Skripte/GlobalConfig.cs
@@ -9,7 +9,7 @@ using System;
 */
 public class GlobalConfig : MonoBehaviour
 {
-    public static string BASE_URL = "http://localhost:8443/api/";
+    public static string BASE_URL = "http://localhost:8080/api/";
     public static float CLIENT_UPDATE_INTERVAL = .05f;
 
     public const bool START_REST_SERVER = false;
